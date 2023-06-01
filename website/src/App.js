447d3e4b691b26bf59e0e1fb743718e0ad9f6b8e
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-06-02 04:49:09 +0530

    on the way

---
+++ website/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+
+    </Router>
+  )
+}
+
+export default App
